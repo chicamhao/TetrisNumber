71f671e01c80359af6b496141a3685c88394cd04
HienTran <49000443+hientranHT@users.noreply.github.com>
2022-01-19 20:38:37 +0700

    Feat: Background

--- Tetris-Number/Assets/Scripts/Configurations.cs
+++ Tetris-Number/Assets/Scripts/Configurations.cs
@@ -4,6 +4,6 @@ public static class Configurations
 {
     public static int NUMBER_SIZE = 212;
 
-    public static Vector3 NumberDroppingVelocity = new Vector3(0f, -.2f, 0f);
+    public static Vector3 NumberDroppingVelocity = new Vector3(0f, -.05f, 0f);
     public static Vector2 NORMAL_BOARD_SIZE = new Vector2(5, 6);
 }
