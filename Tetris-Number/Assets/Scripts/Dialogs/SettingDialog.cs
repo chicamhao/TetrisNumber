3bf19b7ee7b82aa52539ccdc3aaea7238f6368ab
ENISH-04\Enish-04 <hao.chi@enish.com>
2022-02-25 09:13:05 +0700

    refacturate asset folders

---
+++ Tetris-Number/Assets/Scripts/Dialogs/SettingDialog.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingDialog : DialogBase
+{
+    protected override void HideBehaviour()
+    {
+    }
+
+    protected override void ShowBehaviour()
+    {
+    }
+}
